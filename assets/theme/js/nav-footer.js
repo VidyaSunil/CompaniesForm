8f3fb55c64627323f235294e01c8854ff979b055
Weloft Labs <tech@weloftlabs.com>
2017-07-12 23:53:13 +0530

    Prod commit

---
+++ assets/theme/js/nav-footer.js
@@ -0,0 +1,320 @@
+
+var header = '<section class="menu1" id="menu-0">'+
+'			<nav class="navbar navbar-dropdown navbar-fixed-top">'+
+'				<div class="container-fluid">'+
+''+
+'					<div class="mbr-table">'+
+'						<div class="mbr-table-cell">'+
+''+
+'							<div class="navbar-brand">'+
+'								<a href="index.html" class="navbar-logo"><img src="assets/images/cf-logo-transparent.png" alt="Mobirise"></a>'+
+''+
+'							</div>'+
+''+
+'						</div>'+
+'						<div class="mbr-table-cell">'+
+''+
+'							<button class="navbar-toggler pull-xs-right hidden-md-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">'+
+'								<span class="hamburger-icon"></span>'+
+'							</button>'+
+''+
+'							<ul class="nav-dropdown collapse pull-xs-right nav navbar-nav navbar-toggleable-sm" id="exCollapsingNavbar">'+
+'								<li class="nav-item dropdown">'+
+'									<a class="nav-link link dropdown-toggle" data-toggle="dropdown-submenu" href="https://mobirise.com/" aria-expanded="false">Incorporation</a>'+
+'									<div class="dropdown-menu">'+
+'										<a class="dropdown-item" href="start-business.html">Start A Business</a>'+
+'										<a class="dropdown-item" href="foreign-registration.html">Foreign Company Registration</a>'+
+'									</div>'+
+'								</li>'+
+'								<li class="nav-item">'+
+'									<a class="nav-link link" aria-expanded="false" href="allied-registration.html"> Allied Registration </a>'+
+'								</li>'+
+'								<li class="nav-item">'+
+'									<a class="nav-link link" aria-expanded="false" href="manage-business.html"> Manage Business </a>'+
+'								</li>'+
+'								<li class="nav-item dropdown">'+
+'									<a class="nav-link link dropdown-toggle" data-toggle="dropdown-submenu" aria-expanded="false">Startup Corner</a>'+
+'									<div class="dropdown-menu">'+
+'										<a class="dropdown-item" href="cfo-on-call.html">CFO on Call</a>'+
+'										<a class="dropdown-item" href="startup-package.html">Startup Package</a>'+
+'										<a class="dropdown-item" href="legal-documents.html">Legal Documents</a>'+
+'									</div>'+
+'								</li>'+
+'								<li class="nav-item">'+
+'									<a class="nav-link link" aria-expanded="false" href="gst.html"> GST </a>'+
+'								</li>'+
+'								<li class="nav-item">'+
+'									<a class="nav-link link" href="funding.html" aria-expanded="false">Funding</a>'+
+'								</li>'+
+'								<li class="nav-item">'+
+'									<a class="nav-link link" href="contact.html" aria-expanded="false">Contact</a>'+
+'								</li>'+
+'							</ul>'+
+'							<button hidden="" class="navbar-toggler navbar-close" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">'+
+'								<span class="close-icon"></span>'+
+'							</button>'+
+''+
+'						</div>'+
+'					</div>'+
+''+
+'				</div>'+
+'			</nav>'+
+''+
+'		</section>';
+	
+
+var footer = '<footer class="mbr-small-footer mbr-section mbr-section-nopadding footer1" id="footer3-b" style="background-color: #2C3338; padding-top: 1.75rem; padding-bottom: 1.75rem;">'+
+
+'			<div align="right">'+
+'				<p style="font-size: 15px; font-family: \'Montserrat\'; color: #fff" >'+
+'					INTERNATIONAL NETWORK PARTNERS PRESENCE AT : &nbsp;&nbsp;<img src="assets/images/singapore.png" style="height: 15px;">&nbsp;&nbsp; SINGAPORE, &nbsp;&nbsp;<img src="assets/images/mauritius.png" style="height: 15px;">&nbsp;&nbsp;  MAURITIUS, &nbsp;&nbsp;<img src="assets/images/maldives.png" style="height: 15px;">&nbsp;&nbsp;  MALDIVES & &nbsp;&nbsp;<img src="assets/images/uae.png" style="height: 15px;">&nbsp;&nbsp;  DUBAI &nbsp;'+
+'				</p>'+
+'			</div>'+
+
+'			<div class="container-fluid">'+
+'				<div class="row">'+
+'					<div class="col-xs-12 col-lg-12 ">'+
+'						<div class="footer-list col-xs-12 col-lg-12">'+
+'							<div class="col-xs-12 col-md-4 text-xs-center text-md-left">'+
+'								<p class="mbr-section-subtitle display-3" style="font-size: 21px; margin-top: 30px;">'+
+'									Popular Services'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="misc.html#Business">Business Advisories & Business Consulting</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="misc.html#Tax">Tax Regulatory & Advisory Services</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="start-business.html#Private">Private Limited Company Registration</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="start-business.html#Limited">Limited Liability Partnership Registration - LLP</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="start-business.html#One">One Person Company</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="foreign-registration.html">Foreign Company Registration</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="misc.html#Consultancy">Consultancy on Company Law Matters</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="manage-business.html#OGS">Secretarial Practice</a>'+
+'								</p>'+
+''+
+'								<p class="mbr-section-text lead">'+
+'									<a href="misc.html#Annual">Annual Filing – Income Tax Returns</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="misc.html#Filings">Annual Filings – Company Returns</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="manage-business.html#OGS">Conversion of Business Entity</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="allied-registration#Karnataka">Society Registration</a>'+
+'								</p>'+
+''+
+'							</div>'+
+'							<div class="col-xs-12 col-md-4 text-xs-center text-md-left">'+
+'								<p class="mbr-section-subtitle display-3" style="font-size: 21px; margin-top: 30px;">'+
+'									Quick Links'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="http://www.mca.gov.in">Ministry of Corporate Affairs</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="https://www.gst.gov.in">GST</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="https://incometaxindiaefiling.gov.in">Income Tax</a>'+
+'								</p>'+
+''+
+'								<p class="mbr-section-text lead">'+
+'									<a href="http://startupindia.gov.in">Startup Website</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="http://dipp.nic.in">Department of Industrial Policy & Promotion</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="http://msme.gov.in">Ministry of Small & Medium Enterprise</a>'+
+'								</p>'+
+''+
+'							</div>'+
+''+
+'							<div class="col-xs-12 col-md-4 text-xs-center text-md-left">'+
+'								<p class="mbr-section-subtitle display-3" style="font-size: 21px; margin-top: 30px;">'+
+'									Contact Us'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									VidyaSunil & Associates Pvt. Ltd'+
+'									<br>'+
+'									# 64 , Benaka Krupa Apartments'+
+'									<br>'+
+'									7th Main , 7th Cross,'+
+'									<br>'+
+'									Rajeev Nagar'+
+'									<br>'+
+'									Banashankari 3rd Stage'+
+'									<br>'+
+'									Bangalore - 560085'+
+'									<br>'+
+'									E Mail ID : companiesform@gmail.com'+
+'									<br>'+
+'									Cell No. :+91  9739834819 / +91 9480633382'+
+'									<br>'+
+'								</p>'+
+'							</div>'+
+'						</div>'+
+'					</div>'+
+'				</div>'+
+''+
+'				<div class="row" style="margin-top: 20px;">'+
+'					<div class="col-xs-12 col-lg-12 ">'+
+'						<div class="footer-list col-xs-12 col-lg-12">'+
+'							<div class="col-xs-12 col-md-4 text-xs-center text-md-left">'+
+'								<p class="mbr-section-subtitle display-3" style="font-size: 21px; margin-top: 30px;">'+
+'									Startup & SME Popular Services'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="startup-package.html">Startup & SME Compliance Package</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="cfo-on-call.html">CFO On CALL – Integrated Services </a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="funding.html#form1-3p">Startups Funding Registration </a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="funding.html#form1-3p1">Angel Investors / Venture Funding Registration</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="legal-documents.html">Co-Founders Agreements</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="startup-package.html#SMB">SMB’S Tax Compliance Management / Outsourcing Services</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="startup-package.html#finances">Finance for - SMALL & MEDIUM BUSINESS ( SME / MSME) – SMB’s</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="cfo-on-call.html#consult">CFO On CALL - Consultation</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="startup-package.html#Definition">Definition of Startup – As per DIPP</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="startup-package.html#Angel">Angel Tax</a>'+
+'								</p>'+
+'							</div>'+
+'							<div class="col-xs-12 col-md-4 text-xs-center text-md-left">'+
+'								<p class="mbr-section-subtitle display-3" style="font-size: 21px; margin-top: 30px;">'+
+'									Funding'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="funding.html#form1-3p">Startups</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="funding.html#form1-3p1">Investors</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="startup-package.html#finances">Business Loans</a>'+
+'								</p>'+
+''+
+'								<p class="mbr-section-text lead">'+
+'									<a href="startup-package.html#finances">Machinery Equipment Financing</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="startup-package.html#finances">Online Seller Finance</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="startup-package.html#finances">Loan Against Property </a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="startup-package.html#finances">Working Capital Loans</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="startup-package.html#finances">Lease Finance Loans</a>'+
+'								</p>'+
+'							</div>'+
+''+
+'							<div class="col-xs-12 col-md-4 text-xs-center text-md-left">'+
+'								<p class="mbr-section-subtitle display-3" style="font-size: 21px; margin-top: 30px;">'+
+'									Allied Registraitons'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="allied-registration.html#FSSAI">FSSAI</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="allied-registration.html#SSI">Small Scale Industries (SSI)</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="allied-registration.html#MSME">Micro & Small Medium Industries (MSME) Registration</a>'+
+'								</p>'+
+''+
+'								<p class="mbr-section-text lead">'+
+'									<a href="allied-registration.html#Professional">Professional Tax Registration</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="allied-registration.html#Shops">Shops & Establishment Registration </a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="allied-registration.html#Import">Import & Export Code (IEC) Registration</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="allied-registration.html#">NSIC Registration</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="allied-registration.html#Karnataka">Karnataka Societies Registration Act (KSRA)</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="allied-registration.html#Karnataka">Karnataka Apartment Ownership Act (KAOA)</a>'+
+'								</p>'+
+''+
+'							</div>'+
+''+
+'						</div>'+
+''+
+'					</div>'+
+'				</div>'+
+''+
+'				<div class="row second-line">'+
+'					<div class="col-xs-12">'+
+'						<div class="col-md-2">'+
+'							<div class="logo">'+
+'								<a href="index.html"><img src="assets/images/cf-logo-transparent.png" alt="Mobirise"></a>'+
+'							</div>'+
+'						</div>'+
+'						<div class="col-md-4 col-md-offset-6">'+
+'							<div class="col-xs-12 text-xs-center text-md-left">'+
+'								<p class="mbr-section-subtitle display-3">'+
+'									Follow Us'+
+'								</p>'+
+'								<div class="mbr-section-btn">'+
+'									<a class="btn btn-icon" href="https://www.facebook.com/CompaniesForm" target="_blank"> <span class="socicon socicon-facebook mbr-iconfont mbr-iconfont-btn"></span></a>'+
+'									<a class="btn btn-icon" href="https://twitter.com/CompaniesForm" target="_blank"> <span class="socicon socicon-twitter mbr-iconfont mbr-iconfont-btn"></span></a>'+
+'									<a class="btn btn-icon" href="https://www.linkedin.com/in/CompaniesForm" target="_blank"> <span class="socicon socicon-linkedin mbr-iconfont mbr-iconfont-btn"></span></a>'+
+'									<a class="btn btn-icon" href="https://www.tumblr.com/blog/companiesform" target="_blank"> <span class="socicon socicon-tumblr mbr-iconfont mbr-iconfont-btn"></span></a>'+
+'									<a class="btn btn-icon" href="https://www.instagram.com/companiesform/" target="_blank"> <span class="socicon socicon-instagram mbr-iconfont mbr-iconfont-btn"></span></a>'+
+'								</div>'+
+'							</div'+
+'							>'+
+'						</div>'+
+'					</div>'+
+'				</div>'+
+'			</div>'+
+'			<div class="container margin-top-40">'+
+'				<p class="text-xs-center mbr-section-text lead">'+
+'					Copyright (c) 2016 <strong><a href="http://weloftlabs.com" target="_blank">Weloft Labs</a></strong>.'+
+'				</p>'+
+'			</div>'+
+'		</footer>';
+	
+
+
+
+
+document.getElementById('my-nav').innerHTML+=header;
+document.getElementById('my-footer').innerHTML+=footer;
